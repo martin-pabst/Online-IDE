f270fd35cc5d308604c3382d3e1c71ed247f516c
Martin Pabst <asv.pabst@gmail.com>
2021-06-19 20:07:53 +0200

    minor bufix: access to user object after logout caused nullpointer exception

--- src/client/main/Main.ts
+++ src/client/main/Main.ts
@@ -304,7 +304,7 @@ export class Main implements MainBase {
 
         $(window).on('unload', function() {
             
-            if(navigator.sendBeacon){
+            if(navigator.sendBeacon && that.user != null){
                 that.networkManager.sendUpdates(null, false, true);
                 that.networkManager.sendUpdateUserSettings(() => {});
             }

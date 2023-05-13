5a75178af32c09c10905ec78a5cfba0eae1206f5
Martin Pabst <asv.pabst@gmail.com>
2023-05-13 13:51:18 +0200

    include fonts.css in includeIDE.js

--- public/includeIDE.js
+++ public/includeIDE.js
@@ -7,11 +7,13 @@ let base = "https://embed.learnj.de/include/";
 // includeJs(base + "lib/monaco-editor/dev/vs/loader.js");
 // includeJs(base + "js/runtimelibrary/graphics/SpriteLibrary.js");
 // includeJs(base + "lib/howler/howler.core.min.js");
+//
 
 let scripts = [
     // base + "lib/pixijs/pixi.js",
     // base + "lib/pixi3d/pixi3d.js",
     base + "lib/p5.js/p5.js",
+    base + 'assets/fonts/fonts.css',
     base + 'online-ide-embedded.css',
     base + "lib/jquery/jquery-3.3.1.js",
     base + "lib/markdownit/markdownit.min.js",

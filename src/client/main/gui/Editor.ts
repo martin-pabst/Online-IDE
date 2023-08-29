abf533fa688fa107ca93f274d86b2feb01b66ebb
martin-pabst <asv.pabst@gmail.com>
2023-08-29 19:47:05 +0200

    litte bugfix: exception in Editor.ts if model == null

--- src/client/main/gui/Editor.ts
+++ src/client/main/gui/Editor.ts
@@ -275,8 +275,10 @@ export class Editor implements monaco.languages.RenameProvider {
         this.editor.onDidChangeModel((event) => {
 
             let element: HTMLDivElement = <any>$element.find('.monaco-editor')[0];
-            element.removeEventListener("wheel", mouseWheelListener);
-            element.addEventListener("wheel", mouseWheelListener, { passive: false });
+            if(element != null){
+                element.removeEventListener("wheel", mouseWheelListener);
+                element.addEventListener("wheel", mouseWheelListener, { passive: false });
+            }
 
             if (this.main.getCurrentWorkspace() == null) return;
 
